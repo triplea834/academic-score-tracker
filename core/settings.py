43add99c7cdb902e934393723e9bfa61661f0113
Adedotun <adeadeny5@gmail.com>
2025-08-27 14:18:05 +0100

    feat: enable DRF page-number pagination (PAGE_SIZE=10)

--- core/settings.py
+++ core/settings.py
@@ -61,6 +61,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 
+        'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10,
 }
 
 ROOT_URLCONF = 'core.urls'
