4894bc02e1496b71913d95012f2b50ea0441f04d
Adedotun <adeadeny5@gmail.com>
2025-08-27 14:57:04 +0100

    edited core/settings.py

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ar55%od^ywyc$s795#gzxh%#kb^=t&ydh3%4+xxfqiwc=)-l0o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "triplea.pythonanywhere.com", 
+    "127.0.0.1", 
+    "localhost",
+]
 
 
 # Application definition
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import SemesterViewSet, CourseViewSet, summary 
+from . import views
+from .views_csv import CoursesExportCSV, CoursesImportCSV
 
 router = DefaultRouter()
 router.register(r'semesters', SemesterViewSet, basename='semester')
@@ -9,4 +11,6 @@ router.register(r'courses', CourseViewSet, basename='course')
 urlpatterns = [
     path('', include(router.urls)),
     path('summary/', summary, name='summary'),
+    path("courses/export/", CoursesExportCSV.as_view(), name="courses-export"),
+    path("courses/import/", CoursesImportCSV.as_view(), name="courses-import"),
 ]
