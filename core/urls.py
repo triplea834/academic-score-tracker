31c9327a46e44c329ce520d78b07e3cb28b9bbcd
Adedotun <adeadeny5@gmail.com>
2025-08-27 13:38:30 +0100

    docs: add Swagger and ReDoc via drf-yasg

--- core/urls.py
+++ core/urls.py
@@ -16,13 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Academic Score Tracker API",
+        default_version="v1",
+        description="Record semesters, courses, compute GPA/CGPA.",
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Auth endpoints
-    path('api/auth/', include('users.urls')),
-
-    # Tracker endpoints
-    path('api/', include('tracker.urls')),
+    path('api/', include('tracker.urls')), # your trackers urls
+    path('api/auth/', include('users.urls')), # your auth urls
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
